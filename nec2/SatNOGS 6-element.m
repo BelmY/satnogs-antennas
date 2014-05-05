4ee40698ca683e3eb1b5e3ebe60dbec39c51b96e
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2014-05-06 00:13:21 +0300

    nec2: GNU Octave antenna wavelength calculator

---
+++ nec2/SatNOGS 6-element.m
@@ -0,0 +1,80 @@
+#!/usr/bin/octave -qf
+#
+# Yagi-Uda antenna wavelength size calculator
+#
+# Copyright (C) 2014 Vasilis Tsiligiannis <acinonyx@openwrt.gr>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Describe antenna
+#
+# Elements are defined from reflector to directors. First column is
+# the position and second the size of the element. First element
+# position (reflector) should always be zero. All sizes are in
+# Single reflector and single radiator design is assumed.
+
+antenna.elements = [ 0, 1030e-3
+                    260e-3, 1003e-3
+                    395e-3, 952e-3
+                    845e-3, 934e-3
+                    1470e-3, 934e-3
+                    1980e-3, 908e-3 ];
+antenna.diameter = 4e-3;
+
+# Center frequency
+frequency = 144.300e6;
+
+# DO NOT EDIT BELOW!!!
+
+# Constants
+# Speed of light
+c = 299792458;
+
+# Calculate lamda
+lamda = c / frequency;
+
+# Duplicate last column and divide by lamda
+antenna.elements = [ antenna.elements, antenna.elements(:, end) / ...
+                    2] / lamda;
+
+# Check if we have at least 2 elements
+if rows(antenna.elements) < 2
+    error("Antenna with less than two elements");
+endif
+
+# Check if reflector is at position zero
+if antenna.elements(1,1) != 0
+    error("Reflector position is not zero");
+endif
+
+# Print description
+printf("Yagi-Uda antenna wavelength size calculator\n\n");
+
+# Print lamda
+printf("Lambda:\t\t%eλ\n", lamda);
+# Print element diameter
+printf("Diameter:\t%eλ\n\n", antenna.diameter / lamda);
+
+# Print header row
+printf("Element\t\tPosition\tLength\t\tHalf length\n");
+
+# Print reflector
+printf("Reflector:\t%eλ\t%eλ\t%eλ\n", antenna.elements'(:, 1));
+# Print radiator
+printf("Radiator:\t%eλ\t%eλ\t%eλ\n", antenna.elements'(:, 2));
+# Check if directors exist and print them
+if rows(antenna.elements) > 2
+    printf("Director:\t%eλ\t%eλ\t%eλ\n", antenna.elements'(:, ...
+        3:end));
+endif
